6befb52c725f0569d05ec6db13c764ed62c64ab4
vinhintw <11131415@me.mcu.edu.tw>
2025-06-24 14:52:11 +0800

    Remove sessions migration and update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_034903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "sessions", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
@@ -32,6 +23,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_034903) do
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
-
-  add_foreign_key "sessions", "users"
 end
