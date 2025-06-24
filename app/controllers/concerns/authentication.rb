ed90a3e7ae62607af99e594babbcb346c83e2abb
vinhintw <11131415@me.mcu.edu.tw>
2025-06-24 14:51:16 +0800

    Simplify authentication system
    - Replace session-based authentication with Rails session
    - Remove Session model and related ActionCable connection
    - Update Authentication concern to use session[:user_id]
    - Remove sessions association from User model

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -3,7 +3,7 @@ module Authentication
 
   included do
     before_action :require_authentication
-    helper_method :authenticated?
+    helper_method :authenticated?, :current_user
   end
 
   class_methods do
@@ -14,19 +14,18 @@ module Authentication
 
   private
     def authenticated?
-      resume_session
+      current_user.present?
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      session[:user_id] = nil
+      nil
     end
 
     def require_authentication
-      resume_session || request_authentication
-    end
-
-    def resume_session
-      Current.session ||= find_session_by_cookie
-    end
-
-    def find_session_by_cookie
-      Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+      authenticated? || request_authentication
     end
 
     def request_authentication
@@ -39,14 +38,10 @@ module Authentication
     end
 
     def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
-        Current.session = session
-        cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
-      end
+      session[:user_id] = user.id
     end
 
     def terminate_session
-      Current.session.destroy
-      cookies.delete(:session_id)
+      session[:user_id] = nil
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :sessions, dependent: :destroy
 
   # role management
   enum :role, { normal: 0, admin: 1 }
