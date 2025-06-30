4dae5b9190ded8f1fa7299b8e21ac2d0d78b3554
vinhintw <11131415@me.mcu.edu.tw>
2025-07-01 00:39:47 +0700

    feat: add routes for home and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :tasks
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "tasks#index"
+  root "home#index"
 end
