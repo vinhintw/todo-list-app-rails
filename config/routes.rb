0cb2e64e5902fb27081925e8fb5c61e7f63f0439
vinhintw <11131415@me.mcu.edu.tw>
2025-07-01 00:58:15 +0700

    feat: add registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/registration", to: "registrations#new", as: :new_registration
+  post "/registration", to: "registrations#create", as: :registration
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
