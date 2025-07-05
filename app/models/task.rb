31eb4219f745a06db743a9fe3b0b637759d8f5a1
vinhintw <11131415@me.mcu.edu.tw>
2025-07-06 00:35:59 +0800

    Update Task model to use i18n for validation errors

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,7 +17,7 @@ class Task < ApplicationRecord
     return unless start_time.present? && end_time.present?
 
     if end_time <= start_time
-      errors.add(:end_time, "must be after start time")
+      errors.add(:end_time, I18n.t("errors.end_time_after_start"))
     end
   end
 end
