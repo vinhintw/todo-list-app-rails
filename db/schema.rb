33921022486b0f5ed464426be0654be6c50f959a
vinhintw <11131415@me.mcu.edu.tw>
2025-07-07 18:17:07 +0800

    feat: update tasks schema to enforce title presence and add check constraints for data integrity

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_085434) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_07_080753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "start_time"
     t.datetime "end_time"
@@ -25,6 +25,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_085434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.check_constraint "content IS NULL OR length(content) <= 5000", name: "content_length_check"
+    t.check_constraint "length(title::text) <= 255", name: "title_length_check"
+    t.check_constraint "priority = ANY (ARRAY[0, 1, 2, 3])", name: "priority_enum_check"
+    t.check_constraint "start_time IS NULL OR end_time IS NULL OR end_time > start_time", name: "end_time_after_start_time_check"
+    t.check_constraint "status = ANY (ARRAY[0, 1, 2, 3])", name: "status_enum_check"
   end
 
   create_table "users", force: :cascade do |t|
