82afac42a2fedd99a0971fbfc15fa32e8d9d166c
vinhintw <11131415@me.mcu.edu.tw>
2025-07-11 14:12:57 +0800

    feat: add signup routes for registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    get "/signup", to: "registrations#new"
+    post "/signup", to: "registrations#create"
 
     # Defines the root path route ("/")
     root "tasks#index"
