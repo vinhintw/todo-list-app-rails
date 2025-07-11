af35b475e4a7d463f5cb3640b8a85aa335ac88e9
vinhintw <11131415@me.mcu.edu.tw>
2025-07-11 13:28:39 +0800

    refactor: task model specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,39 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  before do
-    @user = User.create(email_address: "test@example.com", password: "password", username: "testuser")
-  end
+  let(:user) { create(:user) }
 
-  it "is valid with valid attributes" do
-    task = Task.new(
-      title: "Test Title",
-      content: "Test content",
-      start_time: DateTime.now,
-      end_time: DateTime.now + 1.hour,
-      user: @user
-    )
-    expect(task).to be_valid
-  end
+  describe "validations" do
+    context "when creating a task" do
+      let(:task) { Task.new(
+        title: "Test Title",
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour,
+        user: user
+      )}
+      it { expect(task).to be_valid }
+    end
 
-  it "is not valid without a title" do
-    task = Task.new(
-      content: "Test content",
-      start_time: DateTime.now,
-      end_time: DateTime.now + 1.hour,
-      user: @user
-    )
-    expect(task).to_not be_valid
-  end
+    context "when leave blank title" do
+      let(:task) { Task.new(
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour,
+        user: user
+      )}
+      it { expect(task).to_not be_valid }
+    end
 
-  it "is not valid if end_time is before start_time" do
-    task = Task.new(
-      title: "Test Title",
-      content: "Test content",
-      start_time: DateTime.now,
-      end_time: DateTime.now - 1.hour,
-      user: @user
-    )
-    expect(task).to_not be_valid
+    context "when start_time is before end_time" do
+      let(:task) { Task.new(
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour,
+        user: user
+      )}
+      it { expect(task).to_not be_valid }
+    end
+
+    context "when end_time is before start_time" do
+      let(:task) { Task.new(
+        title: "Test Title",
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now - 1.hour,
+        user: user
+      )}
+      it { expect(task).to_not be_valid }
+    end
+
+    context "when title is over 255 characters" do
+      let(:task) { Task.new(
+        title: "a" * 256,
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour,
+        user: user
+      )}
+      it { expect(task).to_not be_valid }
+    end
+
+    context "when content is over 5000 characters" do
+      let(:task) { Task.new(
+        title: "Test Title",
+        content: "a" * 5001,
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour,
+        user: user
+      )}
+      it { expect(task).to_not be_valid }
+    end
+
+    context "when user is not present" do
+      let(:task) { Task.new(
+        title: "Test Title",
+        content: "Test content",
+        start_time: DateTime.now,
+        end_time: DateTime.now + 1.hour
+      )}
+      it { expect(task).to_not be_valid }
+    end
   end
 end
