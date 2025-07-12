4b594b8856e584e3fd0479ac9b1556c31164ff2d
vinhintw <11131415@me.mcu.edu.tw>
2025-07-12 12:01:09 +0800

    feat: test task search filtering with title and status conditions

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -79,23 +79,28 @@ RSpec.describe Task, type: :model do
     end
   end
 
-  describe 'test search condition of task title' do
-    let!(:task1) { create(:task, user: user, title: 'Task 1') }
-    let!(:task2) { create(:task, user: user, title: 'Task 2') }
-    let!(:task3) { create(:task, user: user, title: 'Different') }
+  describe 'search filtering tasks' do
+    let!(:task1) { create(:task, user: user, title: 'Task 1', status: 'in_progress') }
+    let!(:task2) { create(:task, user: user, title: 'Task 2', status: 'completed') }
+    let!(:task3) { create(:task, user: user, title: 'Different', status: 'cancelled') }
     let(:title_params) { { title: 'Task' } }
 
-    before do
-      @q = user.tasks.ransack(title_cont: title_params[:title])
+    context 'when searching tasks with title condition' do
+      before do
+        @q = user.tasks.ransack(title_cont: title_params[:title])
+      end
+
+      it { expect(@q.result).to include(task1, task2) }
+      it { expect(@q.result).not_to include(task3) }
     end
 
-    it 'returns tasks that match the title condition' do
-      expect(@q.result).to include(task1, task2)
-      expect(@q.result).not_to include(task3)
-    end
+    context 'when searching tasks with status condition' do
+      before do
+        @q = user.tasks.ransack(status_eq: Task.statuses[:in_progress])
+      end
 
-    it 'does not return tasks that do not match the title condition' do
-      expect(@q.result).not_to include(task3)
+      it { expect(@q.result).to include(task1) }
+      it { expect(@q.result).not_to include(task2, task3) }
     end
   end
 end
