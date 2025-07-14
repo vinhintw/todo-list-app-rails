f9f428d577e1482973d878a144b79e77a0dcae48
vinhintw <11131415@me.mcu.edu.tw>
2025-07-14 18:39:47 +0800

    feat: add destroy action for user management in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -51,6 +51,17 @@ class AdminController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+
+
+    respond_to do |format|
+      format.html { redirect_to admin_path, status: :see_other, notice: t("flash.user_destroyed") }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def set_user
