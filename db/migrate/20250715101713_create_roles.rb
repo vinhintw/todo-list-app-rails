32b38287a1d473d22ed7b4aff3c4a75edb807f63
vinhintw <11131415@me.mcu.edu.tw>
2025-07-15 18:21:57 +0800

    feat: create roles migration with unique name index

---
+++ db/migrate/20250715101713_create_roles.rb
@@ -0,0 +1,12 @@
+class CreateRoles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :roles do |t|
+      t.string :name, null: false
+      t.text :description
+
+      t.timestamps
+    end
+
+    add_index :roles, :name, unique: true
+  end
+end
