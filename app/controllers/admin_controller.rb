7e18641b9ce95ebd3f814ddb6aae4895725eff93
vinhintw <11131415@me.mcu.edu.tw>
2025-07-18 17:12:35 +0800

    feat: add self-deletion restriction in user destroy action and improve role creation success message

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -60,6 +60,14 @@ class AdminController < ApplicationController
   end
 
   def destroy
+    if @user == current_user
+      respond_to do |format|
+        format.html { redirect_to admin_path, alert: t("flash.cannot_delete_self") }
+        format.json { render json: { error: t("flash.cannot_delete_self") }, status: :forbidden }
+      end
+      return
+    end
+
     if @user.admin? && User.joins(:role).where(roles: { name: Role::ADMIN }).count == 1
       respond_to do |format|
         format.html { redirect_to admin_path, alert: t("flash.cannot_delete_last_admin") }
@@ -88,7 +96,7 @@ class AdminController < ApplicationController
   def store_role
     @role = Role.new(role_params)
     if @role.save
-      redirect_to admin_path, notice: t("role.created") + " #{@role.name}"
+      redirect_to admin_path, notice: t("role.created", name: @role.name)
     else
       render :create_role, status: :unprocessable_entity
     end
