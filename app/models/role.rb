0db9e4a6df6c446c9f87386593d9f575d943c425
vinhintw <11131415@me.mcu.edu.tw>
2025-07-18 11:06:47 +0800

    feat: implement Role model with user associations and validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,16 @@
 class Role < ApplicationRecord
+  has_many :users, dependent: :restrict_with_exception
+
+  validates :name, presence: true, uniqueness: true
+
+   ADMIN = "admin".freeze
+   USER = "user".freeze
+
+  def admin?
+    name.downcase == ADMIN
+  end
+
+  def user?
+    name.downcase == USER
+  end
 end
