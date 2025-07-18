65680d7d37138d30f6e9782e515f13da8f6ab636
vinhintw <11131415@me.mcu.edu.tw>
2025-07-18 15:16:46 +0800

    feat: update schema to include taggings and tags tables with foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_18_110000) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_18_110002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -21,6 +21,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_18_110000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["task_id", "tag_id"], name: "index_taggings_on_task_id_and_tag_id", unique: true
+    t.index ["task_id"], name: "index_taggings_on_task_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -55,6 +71,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_18_110000) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "tasks"
   add_foreign_key "tasks", "users"
   add_foreign_key "users", "roles"
 end
