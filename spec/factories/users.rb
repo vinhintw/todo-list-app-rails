8ae0fe8e709494a4463247e39021cc5a4fec305a
vinhintw <11131415@me.mcu.edu.tw>
2025-07-18 12:50:02 +0800

    feat: update user factory to use Role model associations for role assignment

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,18 @@ FactoryBot.define do
     username { Faker::Internet.username(specifier: 5..15) }
     email_address { Faker::Internet.email }
     password { "password123" }
-    role { :normal }
+    role { Role.find_or_create_by(name: "user") }
 
     trait :admin do
-      role { :admin }
+      role { Role.find_or_create_by(name: "admin") }
     end
+  end
+
+  factory :role, aliases: [ :user_role ] do
+    name { "user" }
+  end
+
+  factory :admin_role, class: Role do
+    name { "admin" }
   end
 end
