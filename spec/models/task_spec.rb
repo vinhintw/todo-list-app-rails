76c8762e18799618c9f2c237ef7269759a672881
vinhintw <11131415@me.mcu.edu.tw>
2025-07-22 13:56:19 +0800

    refactor: simplify task creation in search filtering specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -80,27 +80,29 @@ RSpec.describe Task, type: :model do
   end
 
   describe 'search filtering tasks' do
-    let!(:task1) { create(:task, user: user, title: 'Task 1', status: 'in_progress') }
-    let!(:task2) { create(:task, user: user, title: 'Task 2', status: 'completed') }
-    let!(:task3) { create(:task, user: user, title: 'Different', status: 'cancelled') }
+    let!(:task1) { create(:task, :in_progress, user: user, title: 'Task 1') }
+    let!(:task2) { create(:task, :completed, user: user, title: 'Task 2') }
+    let!(:task3) { create(:task, :cancelled, user: user, title: 'Different') }
     let(:title_params) { { title: 'Task' } }
 
     context 'when searching tasks with title condition' do
+      let(:q) { user.tasks.ransack(title_cont: title_params[:title]) }
       before do
-        @q = user.tasks.ransack(title_cont: title_params[:title])
+        q.result
       end
 
-      it { expect(@q.result).to include(task1, task2) }
-      it { expect(@q.result).not_to include(task3) }
+      it { expect(q.result).to include(task1, task2) }
+      it { expect(q.result).not_to include(task3) }
     end
 
     context 'when searching tasks with status condition' do
+      let(:q) { user.tasks.ransack(status_eq: Task.statuses[:in_progress]) }
       before do
-        @q = user.tasks.ransack(status_eq: Task.statuses[:in_progress])
+        q.result
       end
 
-      it { expect(@q.result).to include(task1) }
-      it { expect(@q.result).not_to include(task2, task3) }
+      it { expect(q.result).to include(task1) }
+      it { expect(q.result).not_to include(task2, task3) }
     end
   end
 end
