7626888ed3ede43cc77177aad568e0a566e0e8f3
vinhintw <11131415@me.mcu.edu.tw>
2025-07-25 15:17:18 +0800

    fix: update role names in user and role factories to use constants instead of strings

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,18 +3,22 @@ FactoryBot.define do
     username { Faker::Internet.username(specifier: 5..15) }
     email_address { Faker::Internet.email }
     password { "password123" }
-    role { Role.find_or_create_by(name: "user") }
+    role { Role.find_or_create_by(name: Role::USER) }
 
     trait :admin do
-      role { Role.find_or_create_by(name: "admin") }
+      role { Role.find_or_create_by(name: Role::ADMIN) }
+    end
+
+    trait :normal do
+      role { Role.find_or_create_by(name: Role::USER) }
     end
   end
 
   factory :role, aliases: [ :user_role ] do
-    name { "user" }
+    name { Role::USER }
   end
 
   factory :admin_role, class: Role do
-    name { "admin" }
+    name { Role::ADMIN }
   end
 end
