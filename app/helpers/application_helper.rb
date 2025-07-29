8340f54fbad37b8638e101b640b8f99bd7574c93
vinhintw <11131415@me.mcu.edu.tw>
2025-07-29 17:32:57 +0800

    refactor: update all_tasks_link_class to check current page before status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,7 +68,7 @@ module ApplicationHelper
   end
 
   def all_tasks_link_class
-    if params[:status].blank?
+    if current_page?(root_path) && params[:status].blank?
       "#{SIDEBAR_LINK_BASE_CLASS} #{ACTIVE_ALL_TASKS_CLASS}"
     else
       "#{SIDEBAR_LINK_BASE_CLASS} #{INACTIVE_SIDEBAR_CLASS}"
